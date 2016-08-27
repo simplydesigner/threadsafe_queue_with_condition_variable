f2eaf64510ccd4a0feda70e9a491882b42eb2a38
simplydesigner <simplydesigner@ya.ru>
2016-08-27 08:15:13 +0300

    Initial Commit

---
+++ threadsafe_queue_with_condition_variable/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  threadsafe_queue_with_condition_variable
+//
+//  Created by simplydesigner on 27/08/16.
+//  Copyright © 2016 JustDevelopers. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
